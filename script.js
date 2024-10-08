a0337b8b285329256a4b26e8c5e9bb6dcacaad04
Pietro <pn.pietro134@gmail.com>
2024-10-08 11:24:40 +0200

    Aggiunta logica immagini

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let turno = false;
+
+
 function CreoTabella(){
     let table = document.createElement("table");
     table.style.borderCollapse = "collapse"; 
@@ -7,7 +10,7 @@ function CreoTabella(){
       for (let j = 0; j < 3; j++) {
         let cell = document.createElement("td");
         cell.id = `cell-${i+1}-${j+1}`; 
-        cell.addEventListener("click", handleCellClick); // Add this line
+        cell.addEventListener("click", handleCellClick); 
         row.appendChild(cell);
       }
       tbody.appendChild(row);
@@ -19,4 +22,21 @@ function CreoTabella(){
 
 function handleCellClick(event) {
   //console.log(`Hai cliccato sulla cella ${event.target.id}`);
+  const cell = event.target; 
+  
+  if(turno == false){
+    const imgV = document.createElement('img'); 
+    imgV.src = 'V.png'; 
+    imgV.style.width = '100%'; 
+    imgV.style.height = 'auto';
+    cell.appendChild(imgV);  
+    turno = true;  
+  }else{
+    const imgX = document.createElement('img'); 
+    imgX.src = 'X.png'; 
+    imgX.style.width = '100%'; 
+    imgX.style.height = 'auto';
+    cell.appendChild(imgX);    
+    turno = false; 
+  }
 }
